34ad5a8cec504ca2deac45deec25896cb689b9c5
Luis <denic@yahoo.com>
2021-08-22 12:40:31 -0700

    Only reason flask app was not working was due to not importing 'Response'

--- app.py
+++ app.py
@@ -1,29 +1,37 @@
 import os 
-from flask import Flask, render_template
-
-# import camera driver
-#if os.environ.get('CAMERA'):
- #   Camera = import_module('camera_' + os.environ['CAMERA']).Camera
-#else:
-from kinect import Kinect360 
+from flask import Flask, render_template, Response 
+#from kinect import Kinect360
+import cv2
 
 app = Flask(__name__)
+#deviceNumber depends on the source of your video camera
+deviceNumber = 1
+camera = cv2.VideoCapture(deviceNumber)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
-#kinect1 = Kinect360()
-#kinect1.play_video_save_images()
+def get_frames():
+	while 1:
+		success,frame=camera.read()
+		if not success:
+			break
+		else:
+			ret,buffer = cv2.imencode('.jpg',frame)
+			frame=buffer.tobytes()
+		yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n'+ frame + b'\r\n')
+
 
 #def gen(Kinect360):
 #	while 1:
-#		frame = Kinect360.get_frames()
+#		frame = Kinect360.get_video()
+		
 #		yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n'+ frame + b'\r\n\r\n')
 
-#@app.route('/video_feed')
-#def video_feed():
-#	return Response(gen(Kinect360()), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video')
+def video():
+	return Response(get_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',threaded=True)
